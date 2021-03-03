4c9cd0d614c623034f2d9f01596e7035825f9071
Giritharan <70332966+giri14mct@users.noreply.github.com>
2021-03-03 22:57:10 +0530

    Add files via upload
    initial commit

---
+++ hex.js
@@ -0,0 +1,18 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  let a = "#";
+  for (let i = 0; i < 6; i++) {
+    a += hex[get()];
+  }
+  // console.log(hexColor);
+
+  color.textContent = a;
+  document.body.style.backgroundColor = a;
+});
+
+function get() {
+  return Math.floor(Math.random() * hex.length);
+}
